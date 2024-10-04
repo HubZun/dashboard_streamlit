52857825664e7f5b252225d907b1355ff7279933
NiwwBee <135802604+HubZun@users.noreply.github.com>
2024-10-04 16:07:56 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # Load datasets
-day_df = pd.read_csv("../data/day.csv")
-hour_df = pd.read_csv("../data/hour.csv")
+day_df = pd.read_csv("day.csv")
+hour_df = pd.read_csv("hour.csv")
 
 # Set page title
 st.title("Dashboard Analisis Data Penyewaan Sepeda")
